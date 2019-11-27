bac96d26937ab1d3f854f9b1a89daf97523c0be4
David Soria Parra <dsp@experimentalworks.net>
2019-11-27 01:01:33 -0800

    navigation: make path a iterator

--- src/navigation.rs
+++ src/navigation.rs
@@ -1,7 +1,34 @@
 use crate::types;
 use pathfinding::prelude::dijkstra;
 
-pub type Path = Vec<types::SystemId>;
+pub struct Path<'a> {
+    path: Vec<types::SystemId>,
+    cur: usize,
+    universe: &'a types::Universe,
+}
+
+impl<'a> Path<'a> {
+    pub(self) fn new(universe: &'a types::Universe, path: Vec<types::SystemId>) -> Self {
+        Self {
+            path,
+            universe,
+            cur: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for Path<'a> {
+    type Item = &'a types::System;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur >= self.path.len() {
+            return None;
+        }
+        let system_id = &self.path[self.cur];
+        self.cur += 1;
+        Some(&self.universe.systems[&system_id])
+    }
+}
 
 struct PathBuilder<'a> {
     universe: &'a types::Universe,
@@ -28,7 +55,7 @@ impl<'a> PathBuilder<'a> {
     // TODO: We need to include the Connection itself, otherwise connections can be
     // ambiguous in the rare case that a wormhole leads to the same system next door.
     // In practise it likely doesn't matter.
-    pub fn build(self) -> Option<Path> {
+    pub fn build(self) -> Option<Path<'a>> {
         type Cost = u32;
         // fn successor(system: &types::System) -> Vec<types::System> {
         let u = &self.universe;
@@ -46,28 +73,33 @@ impl<'a> PathBuilder<'a> {
             return None;
         }
 
-        let mut p = Vec::new();
-        for sl in self.waypoints.windows(2) {
-            let a = &sl[0];
-            let b = &sl[1];
+        let mut result = Vec::new();
+        for systems_slice in self.waypoints.windows(2) {
+            let a = &systems_slice[0];
+            let b = &systems_slice[1];
+            // we operate only on system ids
             let (np, _) = dijkstra(&a.id, successor, |id: &types::SystemId| *id == b.id).unwrap();
-            p.extend(np);
+            result.extend(np);
         }
 
-        None
-        // Some(p)
+        Some(Path::new(self.universe, result))
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::database::DatabaseBuilder;
     use std::env;
 
     #[test]
     fn test_me() {
         let uri = env::var("DATABASE_URL").expect("expected env variable DATABASE_URL set");
-        let universe = DatabaseBuilder::new(&uri).build();
-        //     .with_connections(vec![Connection])
+        let universe = DatabaseBuilder::new(&uri).build().unwrap();
+        let path = PathBuilder::new(&universe)
+            .waypoint(&universe.systems[&types::SystemId(30000142)]) // jita
+            .waypoint(&universe.systems[&types::SystemId(30000049)]) // camal
+            .build();
+        println!("{:#?}", path.unwrap().collect::<Vec<_>>());
     }
 }
