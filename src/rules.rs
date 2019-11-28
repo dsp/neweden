efbfe3ea7a852bebb6afe757ed96d8cb2cb6008b
David Soria Parra <dsp@experimentalworks.net>
2019-11-27 22:54:05 -0800

    common: add rules module

---
+++ src/rules.rs
@@ -0,0 +1,12 @@
+use crate::types;
+
+pub fn allows_cynos(system: &types::System) -> bool {
+    let sec_class = types::SecurityClass::from(system.security.clone());
+    let sys_class = types::SystemClass::from(system);
+    match (sys_class, sec_class) {
+        (types::SystemClass::KSpace, types::SecurityClass::Highsec) => false,
+        (types::SystemClass::KSpace, types::SecurityClass::Lowsec) => true,
+        (types::SystemClass::KSpace, types::SecurityClass::Nullsec) => true,
+        (types::SystemClass::WSpace, _) => false,
+    }
+}
