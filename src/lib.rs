a982433b29a7da18d2c1cad20edbec7e034c63f9
David Soria Parra <dsp@experimentalworks.net>
2019-12-24 22:44:57 -0800

    Fix handling of features in neweden common

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,13 @@
 #[cfg(feature = "database")]
 #[macro_use]
 extern crate diesel;
+#[cfg(feature = "database")]
+pub mod database;
+#[cfg(feature = "database")]
+#[allow(non_snake_case)]
+mod schema;
 
 pub use types::*;
-
 #[allow(dead_code)]
 mod types;
 #[allow(dead_code)]
@@ -18,12 +22,6 @@ pub mod rules;
 
 #[allow(dead_code)]
 pub mod navigation;
-
-#[cfg(feature = "database")]
-pub mod database;
-#[cfg(feature = "database")]
-#[allow(non_snake_case)]
-mod schema;
 
 #[cfg(test)]
 mod tests {
