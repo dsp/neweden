8449f09d98679bbb32765bc20303c85add4220cd
David Soria Parra <dsp@experimentalworks.net>
2019-12-27 22:54:39 -0800

    neweden: add a documentation header for the module

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,17 @@
  * Copyright (c) 2019. David "Tiran'Sol" Soria Parra
  * All rights reserved.
  */
-
 #![cfg_attr(test, feature(test))]
+//! neweden is a rust library for system information, wayfinding and
+//! range queries for the MMORPG Eve Online from CCP Games.
+//!
+//! Online data can come from multiple data sources. Most commonly
+//! a CCP static dump from https://www.fuzzwork.co.uk/dump/.
+//!
+//! The library must be compiled with the apprioriate flags. Currently
+//! accepted flags are `database` and `rpc`. `database offers a Postgres
+//! backend using the diesel ORM wrapper. `rpc` is for internal use at
+//! the moment as the dependent crate is not open sourced.
 
 // Must be at the crate root
 #[cfg(feature = "database")]
